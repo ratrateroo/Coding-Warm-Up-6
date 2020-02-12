610c1e99e0ed6916d676ac2fd0ff2fb77afa6c96
Mark Leo <ratrateroo@gmail.com>
2020-02-13 01:01:23 +0800

    updated the script for the videos

--- scripts.js
+++ scripts.js
@@ -1,23 +1,45 @@
-// const videoClickHandler = (video) => {
-//     document.getElementById("slider").src = video;
+// const videoClickHandler = (i) => {
+//     document.getElementById("slider").src = videos[i];
 // };
 
 const videos = [
-    './vidoes/video1.mp4',
-    './vidoes/video2.mp4',
-    './vidoes/video3.mp4',
-    './vidoes/video4.mp4'
+    './videos/video1.mp4',
+    './videos/video2.mp4',
+    './videos/video3.mp4',
+    './videos/video4.mp4'
 ];
 
 
-const videoHandler = (link,i) => {
-    console.log(link, i);
+const videoHandler = (i,e) => {
+    e.preventDefault();
+    const video = document.querySelector('video');
+    const source = document.querySelector('source');
+    //slider.setAttribute('src',videos[i]);
+    console.log(slider);
+    //slider.src = videos[i];
+    
+
+    
+    video.pause();
+  source.setAttribute('src', videos[i]);
+  video.load();
+  video.play();
+
+   
+    
+    
+    
+    
 }
 
-const links = document.querySelectorAll('.navigation li img');
+const links = document.querySelectorAll('.navigation li');
 
 links.forEach((link, i) => {
-    link.addEventListener("click", () => {
-        videoHandler(link,i);
+    link.addEventListener("click", (e) => {
+        
+        console.log(e);
+        console.log(videos[i]);
+        videoHandler(i,e);
+        //videoClickHandler(i);
     });
 });
