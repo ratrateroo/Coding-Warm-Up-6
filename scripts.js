e48c441cfc306d4f5ad62a3fffbfea050c232ed6
Mark Leo <ratrateroo@gmail.com>
2020-02-12 23:35:16 +0800

    upgrading script

--- scripts.js
+++ scripts.js
@@ -1,3 +1,23 @@
 const videoClickHandler = (video) => {
     document.getElementById("slider").src = video;
 };
+
+const videos = [
+    './vidoes/video1.mp4',
+    './vidoes/video2.mp4',
+    './vidoes/video3.mp4',
+    './vidoes/video4.mp4'
+];
+
+
+const videoHandler = (link,i) => {
+    console.log(vidoes[i]);
+}
+
+const links = document.querySelectorAll('.navigation li img');
+
+links.forEach((link, i) => {
+    link.addEventListener("click", () => {
+        videoHandler(link,i);
+    });
+});
